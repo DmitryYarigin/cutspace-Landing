aa95f242c19bf01080c26743e05150b8361e0d94
unknown <yarich9999@gmail.com>
2022-10-22 12:11:58 +0300

    feat: finish header

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// доварление тени в фокусе инпуту
+
+const form = document.querySelector(".form")
+const formInput = document.querySelector('.form__input');
+
+formInput.addEventListener('focus', () => {
+    form.classList.add("form--active")
+})
+
+formInput.addEventListener('blur', () => {
+    form.classList.remove("form--active")
+})
+
